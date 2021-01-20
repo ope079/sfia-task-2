56db2fbdf4a9429853c0088656fdb4ef384216d3
ope079 <o_orekoya@yahoo.com>
2021-01-20 18:04:45 +0000

    changed nginx & docker config

--- jenkins/deploy_stack.sh
+++ jenkins/deploy_stack.sh
@@ -4,4 +4,5 @@ ssh -i ~/.ssh/ansible_id_rsa jenkins@swarm-manager << EOF
     export DATABASE_URI=${DATABASE_URI}
     export SECRET_KEY=${SECRET_KEY}
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml sfia-task-2
+    docker service create --replicas 2 --publish 9000:9000 --name python-http-server ope079/python-http-server
 EOF
