b9cbc51c11bf9d884ebce7caee8a47dd35ebd5ab
ope079 <o_orekoya@yahoo.com>
2021-01-22 14:14:41 +0000

    corrected error in stack_deploy.sh

--- jenkins/deploy_stack.sh
+++ jenkins/deploy_stack.sh
@@ -4,6 +4,6 @@ ssh -i ~/.ssh/ansible_id_rsa jenkins@swarm-manager << EOF
     export DATABASE_URI=${DATABASE_URI}
     export SECRET_KEY=${SECRET_KEY}
     export replicas=${replicas}
-    export app_version=${app_version}
+    export version=${version}
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml sfia-task-2
 EOF
